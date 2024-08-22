86c0c48ebc486aa27cdd1cb1e9f4cab6573208f6
Hafsa10101 <Hafsashk.tech@gmail.com>
2024-08-22 08:27:45 +0530

    Edit external.js
    Added three ways variables can be declared - let, const & var, var is
    generally not used anymore.

---
+++ JS/external.js
@@ -0,0 +1,22 @@
+// Variables
+// let keyword is used to declare variables
+let name = "Hafsa";
+let surname = "Shaikh";
+
+console.log(name);
+console.log(surname);
+
+let age = 22;
+console.log(age); // output: 22
+
+age = 0; // re-assigning variable
+console.log(age); // output: 0
+
+// const keyword to declare constant variables: whole value cannot be changed
+const pi = 3.14;
+// pi = 4; // this will throw error, as re-assignment cannot be done to constant variables
+console.log(pi);
+
+// var keyword to declare variables: it is same as let but it is not used anymore
+var firstName = "Mavie";
+console.log(firstName);
