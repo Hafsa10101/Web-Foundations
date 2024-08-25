e733dc9deccb5f6bd78f10a3c77dbfa57b1dcfd9
Hafsa10101 <Hafsashk.tech@gmail.com>
2024-08-25 10:43:51 +0530

    Edit script.js
    Added code for switch statement.

--- JS/02-datatypes-and-conditionals/script.js
+++ JS/02-datatypes-and-conditionals/script.js
@@ -224,3 +224,33 @@ if(marks >= 75){
 // }else{
 //     console.log("I don't know you");
 // }
+
+
+// switch
+let dayNum = 1;
+
+switch(dayNum){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid Number");
+}
